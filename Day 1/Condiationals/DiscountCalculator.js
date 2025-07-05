bcd48b18ca85920699875be514fa65231528652f
kshitijMandave <kshitij.mandwe@gmail.com>
2025-07-05 23:14:43 +0530

    code updated

--- Day 1/Condiationals/DiscountCalculator.js
+++ Day 1/Condiationals/DiscountCalculator.js
@@ -1,14 +1,31 @@
-let amount = Number(prompt("What is the final amount ?"));
-console.log("Enterd amount is : ", amount);
+// let amount = Number(prompt("What is the final amount ?"));
+
+// if (amount > 0 && amount <= 5000) {
+//   console.log(amount);
+// } else if (amount > 5001 && amount <= 7000) {
+//   console.log(amount - Math.floor((5 * amount) / 100));
+// } else if (amount > 7000 && amount <= 9000) {
+//   console.log(amount - Math.floor((10 * amount) / 100));
+// } else if (amount > 9000) {
+//   console.log(amount - Math.floor((20 * amount) / 100));
+// } else {
+//   console.log("Wrong Input");
+// }
+
+// here we are calculating Math.floor((5 * amount) / 100) discount, and here we are subtracting discount amount with amount and getting payable amount amount - Math.floor((5 * amount) / 100)
+
+// Another Way
+
+let amount = Number(prompt("Enter the amount : "));
+let dis = 0;
 if (amount > 0 && amount <= 5000) {
-  console.log(amount);
-} else if (amount > 5000 && amount <= 7000) {
-  console.log(amount - Math.floor((5 * amount) / 100));
-} else if (amount > 7000 && amount <= 9000) {
-  console.log(amount - Math.floor((10 * amount) / 100));
+  dis = 5;
+} else if (amount > 5001 && amount <= 7000) {
+  dis = 10;
 } else if (amount > 9000) {
-  console.log(amount - Math.floor((20 * amount) / 100));
+  dis = 20;
 } else {
-  console.log(isNaN(amount));
+  console.log("Invalid Amount");
 }
-//
+
+console.log(amount - Math.floor(dis * amount) / 100);
